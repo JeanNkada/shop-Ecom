6530f0adc29e980c75ad047f2c011a30043f4749
Jean Nkada <nkadajohn1@gmail.com>
2024-07-23 10:02:24 +0100

    modelisation de l'appli

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class AdminClient(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    
+    
+class AdminProduit(admin.ModelAdmin):
+    list_display = ('name', 'categorie', 'price', 'digital', 'image', 'date_ajout')
+    
+    
+class AdminCatégory(admin.ModelAdmin):
+    list_display = ('name', 'description') 
+    
+
+class AdminCommande(admin.ModelAdmin):
+    list_display = ('client', 'date_commande', 'complete', 'transaction_id', 'status', 'total_trans')
+    
+    
+class AdminCommandeArticle(admin.ModelAdmin):
+    list_display = ('commande', 'produit', 'quantite', 'date_added')
+    
+
+class AdminAddressChipping(admin.ModelAdmin):
+    list_display = ('client', 'commande', 'addresse', 'ville', 'zipcode', 'date_ajout')
+    
+
+
+admin.site.register(Client, AdminClient)
+admin.site.register(Produit, AdminProduit)
+admin.site.register(Category, AdminCatégory)
+admin.site.register(Commande, AdminCommande)
+admin.site.register(CommandeArticle, AdminCommandeArticle)
+admin.site.register(AddressChipping, AdminAddressChipping)
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from . models import *
+
 # Create your views here.
 
 
@@ -9,16 +11,66 @@ def home(request, *args, **kwargs):
     return render(request, 'base.html', context)
 
 def shop(request, *args, **kwargs):
+    produits = Produit.objects.all()
     
-    context = {}
+    context = {
+        'produits' : produits
+    }
+    
     return render(request, 'shop/index.html', context)
 
+
+
+#afficher les articles de l'utlisateur connecté
 def panier(request, *args, **kwargs):
     
-    context = {}
+    if request.user.is_authenticated:
+        
+        client = request.user.client
+        
+        #récupération de la commande en cours ou création d'une nouvelle commande si elle n'existe pas déjà
+        commande, created = Commande.objects.get_or_create(client=client, complete=False)
+        
+        #liste des article liés a cette commande
+        articles = commande.commandearticle_set.all()
+    else:
+        articles = []
+        
+        commande = {
+        'get_panier_total':0,
+        'get_panier_article':0
+        }
+    context = {
+    'articles':articles,
+    'commande':commande
+
+    }
     return render(request, 'shop/panier.html', context)
 
+
+
 def commande(request, *args, **kwargs):
-    
-    context = {}
+    """ Commande """
+    if request.user.is_authenticated:
+        
+        client = request.user.client
+        
+        commande, created = Commande.objects.get_or_create(client=client, complete=False)
+        
+        articles = commande.commandearticle_set.all()
+        
+    else:
+        
+        articles = []
+        
+        commande = {
+            'get_panier_total':0,
+            'get_panier_article':0
+        }
+        
+    context = {
+        'articles':articles,
+        'commande':commande
+    }
+
     return render(request, 'shop/commande.html', context)
